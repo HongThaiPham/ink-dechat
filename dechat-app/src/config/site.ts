9b94ba967d0501f6d7b6830451b855cefb9aec56
leopham <hongthaipro@gmail.com>
2024-10-29 13:16:56 +0000

    add frontend

---
+++ dechat-app/src/config/site.ts
@@ -0,0 +1,17 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Polkadot DeChat",
+  description:
+    "A decentralized chat application built on Polkadot.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/leopham_it",
+    github: "https://github.com/HongThaiPham",
+  },
+};
